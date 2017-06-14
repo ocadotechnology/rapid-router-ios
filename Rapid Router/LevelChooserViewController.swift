93fd550d1af46b8b95e3e574956557acc9fb1766
Niket Shah <masterniket@gmail.com>
2017-06-14 17:46:33 +0100

    Implement Unity messaging and the Level chooser

---
+++ Rapid Router/LevelChooserViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LevelChooserViewController.swift
+//  Rapid Router
+//
+//  Created by Niket Shah on 07/06/2017.
+//  Copyright © 2017 Ocado. All rights reserved.
+//
+
+class LevelChooserViewController: UITableViewController {
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 109
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Level \(indexPath.row + 1)"
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let level = String(indexPath.row)
+        UnitySendMessage("LevelManager", "LevelChangeListener", level)
+    }
+
+}
