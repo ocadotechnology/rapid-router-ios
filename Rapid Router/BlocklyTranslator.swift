221134c079f519b8e358f7e43f58b6fe307df21b
Niket Shah <masterniket@gmail.com>
2017-10-18 19:03:40 +0100

    implement blockly translator so that it understands move_forwards

--- Rapid Router/BlocklyTranslator.swift
+++ Rapid Router/BlocklyTranslator.swift
@@ -17,16 +17,33 @@ struct BlocklyTranslator {
     let rootBlock: Block
 
     func translateToCode() throws -> Code {
-        if rootBlock.name != "Start" {
+        if rootBlock.name != "start" {
             throw TranslatorError.rootBlockIsNotStart
+        }
+        var currentBlock: Block? = rootBlock.nextBlock
+        var instructions: [Instruction] = []
+        while let block = currentBlock {
+            instructions.append(convertToInstruction(block: block))
+            currentBlock = block.nextBlock
         }
         let startMethod = Method.with {
             $0.name = rootBlock.name
-            $0.instructions = []
+            $0.instructions = instructions
         }
         let code = Code.with {
             $0.methods = [startMethod]
         }
         return code
+    }
+
+    private func convertToInstruction(block: Block) -> Instruction {
+        return Instruction.with {
+            switch block.name {
+            case "move_forwards":
+                $0.type = .moveForwards
+            default:
+                break
+            }
+        }
     }
 }
--- Rapid RouterTests/BlocklyTranslatorTests.swift
+++ Rapid RouterTests/BlocklyTranslatorTests.swift
@@ -12,19 +12,70 @@ import SwiftProtobuf
 @testable import Rapid_Router
 
 class BlocklyTranslatorTests: XCTestCase {
+
+    let blockFactory: BlockFactory = {
+        let factory = BlockFactory()
+        try! factory.load(fromJSONPaths: ["CustomBlocks.json"])
+        return factory
+    }()
     
     func testStartMethod() {
-        // When
-        let block = try! BlockBuilder(name: "Start").makeBlock()
-        let translator = BlocklyTranslator(rootBlock: block)
+        // Given
+        let block = try! blockFactory.makeBlock(name: "start")
         let startMethod = Method.with {
-            $0.name = "Start"
+            $0.name = "start"
             $0.instructions = []
         }
         var expectedCode = Code()
         expectedCode.methods = [startMethod]
+
+        let translator = BlocklyTranslator(rootBlock: block)
         
+        // When
+        let actualCode = try! translator.translateToCode()
+
+        // Then
+        XCTAssertEqual(expectedCode, actualCode)
+    }
+
+    func testRootBlockNotStart() {
         // Given
+        var exceptionCaught = false
+        let block = try! BlockBuilder(name: "Not Start").makeBlock()
+        let translator = BlocklyTranslator(rootBlock: block)
+
+        // When
+        do {
+            _ = try translator.translateToCode()
+        } catch TranslatorError.rootBlockIsNotStart {
+            exceptionCaught = true
+        } catch {
+
+        }
+
+        // Then
+        XCTAssertTrue(exceptionCaught)
+    }
+
+    func testMoveForwardProgram() {
+        // Given
+        let startBlock = try! blockFactory.makeBlock(name: "start")
+        let moveForwardsBlock = try! blockFactory.makeBlock(name: "move_forwards")
+        try! startBlock.nextConnection?.connectTo(moveForwardsBlock.previousConnection)
+
+        let moveForwardsInstruction = Instruction.with {
+            $0.type = .moveForwards
+        }
+        let startMethod = Method.with {
+            $0.name = "start"
+            $0.instructions = [moveForwardsInstruction]
+        }
+        var expectedCode = Code()
+        expectedCode.methods = [startMethod]
+
+        let translator = BlocklyTranslator(rootBlock: startBlock)
+
+        // When
         let actualCode = try! translator.translateToCode()
 
         // Then
